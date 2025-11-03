09e8d1cff32428f769458dac310fdbe0e1fc788b
Rasmus Mikaelsson <ralle.mikaelsson@hotmail.com>
2025-11-03 09:57:15 +0100

    all green on labres. Shorter main with comments

---
+++ Session.vim
@@ -0,0 +1,127 @@
+let SessionLoad = 1
+let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-1 siso=-1
+let v:this_session=expand("<sfile>:p")
+silent only
+silent tabonly
+cd ~/shared/ou1sys
+if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
+  let s:wipebuf = bufnr('%')
+endif
+let s:shortmess_save = &shortmess
+if &shortmess =~ 'A'
+  set shortmess=aoOA
+else
+  set shortmess=aoO
+endif
+badd +113 mexec.c
+badd +2 commands.txt
+badd +85 term://~/shared/ou1sys//111524:/bin/bash
+badd +50 command.c
+argglobal
+%argdel
+$argadd mexec.c
+edit mexec.c
+let s:save_splitbelow = &splitbelow
+let s:save_splitright = &splitright
+set splitbelow splitright
+wincmd _ | wincmd |
+vsplit
+1wincmd h
+wincmd w
+wincmd _ | wincmd |
+split
+1wincmd k
+wincmd w
+let &splitbelow = s:save_splitbelow
+let &splitright = s:save_splitright
+wincmd t
+let s:save_winminheight = &winminheight
+let s:save_winminwidth = &winminwidth
+set winminheight=0
+set winheight=1
+set winminwidth=0
+set winwidth=1
+wincmd =
+argglobal
+setlocal foldmethod=manual
+setlocal foldexpr=0
+setlocal foldmarker={{{,}}}
+setlocal foldignore=#
+setlocal foldlevel=0
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldenable
+silent! normal! zE
+let &fdl = &fdl
+let s:l = 63 - ((21 * winheight(0) + 20) / 41)
+if s:l < 1 | let s:l = 1 | endif
+keepjumps exe s:l
+normal! zt
+keepjumps 63
+normal! 0
+wincmd w
+argglobal
+if bufexists(fnamemodify("commands.txt", ":p")) | buffer commands.txt | else | edit commands.txt | endif
+if &buftype ==# 'terminal'
+  silent file commands.txt
+endif
+balt mexec.c
+setlocal foldmethod=manual
+setlocal foldexpr=0
+setlocal foldmarker={{{,}}}
+setlocal foldignore=#
+setlocal foldlevel=0
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldenable
+silent! normal! zE
+let &fdl = &fdl
+let s:l = 2 - ((1 * winheight(0) + 10) / 20)
+if s:l < 1 | let s:l = 1 | endif
+keepjumps exe s:l
+normal! zt
+keepjumps 2
+normal! 08|
+wincmd w
+argglobal
+if bufexists(fnamemodify("term://~/shared/ou1sys//111524:/bin/bash", ":p")) | buffer term://~/shared/ou1sys//111524:/bin/bash | else | edit term://~/shared/ou1sys//111524:/bin/bash | endif
+if &buftype ==# 'terminal'
+  silent file term://~/shared/ou1sys//111524:/bin/bash
+endif
+balt commands.txt
+setlocal foldmethod=manual
+setlocal foldexpr=0
+setlocal foldmarker={{{,}}}
+setlocal foldignore=#
+setlocal foldlevel=0
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldenable
+let s:l = 95 - ((19 * winheight(0) + 10) / 20)
+if s:l < 1 | let s:l = 1 | endif
+keepjumps exe s:l
+normal! zt
+keepjumps 95
+normal! 028|
+wincmd w
+3wincmd w
+wincmd =
+tabnext 1
+if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0 && getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
+  silent exe 'bwipe ' . s:wipebuf
+endif
+unlet! s:wipebuf
+set winheight=1 winwidth=20
+let &shortmess = s:shortmess_save
+let &winminheight = s:save_winminheight
+let &winminwidth = s:save_winminwidth
+let s:sx = expand("<sfile>:p:r")."x.vim"
+if filereadable(s:sx)
+  exe "source " . fnameescape(s:sx)
+endif
+let &g:so = s:so_save | let &g:siso = s:siso_save
+set hlsearch
+nohlsearch
+doautoall SessionLoadPost
+unlet SessionLoad
+" vim: set ft=vim :
--- mexec.c
+++ mexec.c
@@ -11,8 +11,9 @@
  *                  ./mexec (for stdin input)
  * 
  * Version:         2025-09-11
- *                  2025-09-15 (intermediate version)
- *                  2025-09-16 (final version)
+ *                  2025-09-15
+ *                  2025-09-16
+ *                  2025-11-03 (current version)
  * 
  * Author:          Rasmus Mikaelsson (et24rmn)
  */
@@ -37,27 +38,27 @@
 
 static void redirect_input(int prev_fd);
 static char **parse_line(char *line);
+static int execute_pipeline(char **commands, int cmd_count, FILE *file);
+static int wait_for_children(int cmd_count);
 
 /* -------------------------- External functions -------------------------- */
 
 /**
- * Main program
- * Reads commands from a file and executes them in a pipeline
- * 
+ * main - Reads commands from a file and executes them in a pipeline. 
+ * If no file given, commands will be given trough stdin.
+ *
  * @param argc Argument count
  * @param argv Argument vector
  * @return 0 on success, 1 on failure (EXIT_FAILURE)
  */
 int main(int argc, char *argv[]) {
-    char *filename;
-    FILE *file = NULL;
+    FILE *file = stdin;
    
     if (argc == 2) {
-        filename = argv[1];
-        file = fopen(filename, "r");
+        file = fopen(argv[1], "r");
         validate_file(file);
-    } else {
-        fprintf(stderr, RED "Argument error." RESET " Usage: %s [filename]\n" , argv[0]);
+    } else if(argc > 2) {
+        fprintf(stderr, RED "Too many arguments." RESET " Usage: %s [filename]\n" , argv[0]);
 		fclose(file);
         exit(EXIT_FAILURE);
     }
@@ -69,74 +70,8 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	// Loop the given commands
-    int prev_fd = -1;
-    for (int i = 0; i < cmd_count; i++) {
-        int child_to_child[2] = {-1, -1};
-        pid_t pid;
-
-        // Create pipe if not last command
-        if (i < cmd_count - 1) {
-            if (pipe(child_to_child) == -1) {
-                perror("pipe");
-                free_commands(commands, cmd_count);
-                exit(EXIT_FAILURE);
-            }
-        }
-
-		// Fork a new process
-        pid = fork();
-        if (pid < 0) {
-            perror("fork");
-            if (child_to_child[READ_END] != -1) close(child_to_child[READ_END]);
-            if (child_to_child[WRITE_END] != -1) close(child_to_child[WRITE_END]);
-            free_commands(commands, cmd_count);
-			fclose(file);
-            exit(EXIT_FAILURE);
-        }
-
-        if (pid == 0) {
-            // Child process
-            char **parsed_line = parse_line(commands[i]);
-            if (!parsed_line) {
-                fprintf(stderr, RED "Failed to parse command: %s\n" RESET, commands[i]);
-				free_commands(commands, cmd_count);
-				fclose(file);
-                exit(EXIT_FAILURE);
-            }
-			
-            redirect_input(prev_fd);
-
-            int is_last = (i == cmd_count - 1);
-            if (!is_last) {
-                if(execute_command(child_to_child, parsed_line, commands, cmd_count) > 0) {
-					exit(EXIT_FAILURE);
-				}
-            } else {
-                validate_non_negative(execvp(parsed_line[0], parsed_line));
-            }
-        } else {
-            // Parent process
-            if (prev_fd != -1) close(prev_fd);
-            if (i < cmd_count - 1) {
-                close(child_to_child[WRITE_END]);
-                prev_fd = child_to_child[READ_END];
-            }
-        }
-    }
-    if (prev_fd != -1) close(prev_fd); // Close last read end if open
-
-    int exit_code = 0;
-    for (int i = 0; i < cmd_count; i++) {
-        int status;
-        if (wait(&status) == -1) {
-            perror("waitpid");
-            exit(EXIT_FAILURE);
-        }
-        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-            exit_code = WEXITSTATUS(status);
-        }
-    }
+	execute_pipeline(commands, cmd_count, file);
+    int exit_code = wait_for_children(cmd_count);
 
     // Free allocated memory at the end
     free_commands(commands, cmd_count);
@@ -180,4 +115,84 @@ static char **parse_line(char *line) {
     return tmp;
 }
 
+/**
+ * execute_pipeline - Forking a new child processes for each command
+ * and then executes the given command.
+ *
+ * @param commands		List of commands to be executed
+ * @param cmd_count		Number of commands
+ * @param file			Open file
+ * @return				0 on success, non-zero otherwise
+ */
+static int execute_pipeline(char **commands, int cmd_count, FILE *file) {
+	int prev_fd = -1;
+
+	for(int i = 0; i < cmd_count; i++) {
+		int child_to_child[2] = {-1, -1};
+
+        if (i < cmd_count - 1 && pipe(child_to_child) == -1) {
+            perror("pipe");
+            free_commands(commands, cmd_count);
+            exit(EXIT_FAILURE);
+        }
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            free_commands(commands, cmd_count);
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {
+            // Child
+            char **parsed_line = parse_line(commands[i]);
+            if (!parsed_line) {
+                fprintf(stderr, RED "Failed to parse command: %s\n" RESET, commands[i]);
+                exit(EXIT_FAILURE);
+            }
+
+            redirect_input(prev_fd);
+
+            if (i < cmd_count - 1) {
+                if (execute_command(child_to_child, parsed_line, commands, cmd_count) > 0)
+                    exit(EXIT_FAILURE);
+            } else {
+                validate_non_negative(execvp(parsed_line[0], parsed_line));
+            }
+        } else {
+            // Parent
+            if (prev_fd != -1) close(prev_fd);
+            if (i < cmd_count - 1) {
+                close(child_to_child[WRITE_END]);
+                prev_fd = child_to_child[READ_END];
+            }
+        }
+    }
+
+    if (prev_fd != -1) close(prev_fd);
+    return 0;
+}
+
+/**
+ * wait_for_children - Parrent process, waiting for children to finish
+ * execution of the given commands
+ *
+ * @param cmd_count		Number of commands
+ * @return				0 on sucess, non-zero otherwise
+ */
+static int wait_for_children(int cmd_count) {
+	int exit_code = 0;
+    for (int i = 0; i < cmd_count; i++) {
+        int status;
+        if (wait(&status) == -1) {
+            perror("waitpid");
+            exit(EXIT_FAILURE);
+        }
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+            exit_code = WEXITSTATUS(status);
+        }
+    }
+    return exit_code;
+}
 
